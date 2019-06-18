f09d5d599962b1e0d0a8dcdd19f6b1352a7cf3cd
Hoang-Minh <quataoxanh83@gmail.com>
2019-06-17 21:04:47 -0700

    Add moment to log Time since created campground and comment

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -23,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.use(flash());
+app.locals.moment = require("moment");
 
 //Passport configuration
 app.use(
