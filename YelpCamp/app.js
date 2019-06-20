bec11dec98bd075e2a20f3659057afde8dcc866a
Hoang-Minh <quataoxanh83@gmail.com>
2019-06-20 00:04:41 -0700

    Add nodemailer to reset password

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -17,7 +17,8 @@ var express = require("express"),
 //seedDb();
 mongoose.connect("mongodb://localhost/yelp_camp", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useCreateIndex: true
 });
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
--- YelpCamp/routes/index.js
+++ YelpCamp/routes/index.js
@@ -7,116 +7,230 @@ var nodemailer = require("nodemailer");
 var crypto = require("crypto");
 
 // root route
-router.get("/", function(req, res){
-    res.render("landing");
+router.get("/", function(req, res) {
+  res.render("landing");
 });
 
 //show register form
-router.get("/register", function(req, res){
-    res.render("register");
+router.get("/register", function(req, res) {
+  res.render("register");
 });
 
 //handle signup logic
-router.post("/register", function(req, res){    
-    var newUser = new User({
-        username: req.body.username,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.emmail,
-        avatar: req.body.avatar,
-        isAdmin: req.body.adminCode === process.env.ADMIN_CODE
-    });
-    
+router.post("/register", function(req, res) {
+  var newUser = new User({
+    username: req.body.username,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    avatar: req.body.avatar,
+    isAdmin: req.body.adminCode === process.env.ADMIN_CODE
+  });
 
-    User.register(newUser, req.body.password, function(err, user){
-        if(err || !user){
-            console.log(err);
-            req.flash("error", err.message);
-            return res.render("register");
-        }
+  User.register(newUser, req.body.password, function(err, user) {
+    if (err || !user) {
+      console.log(err);
+      req.flash("error", err.message);
+      return res.render("register");
+    }
 
-        passport.authenticate("local")(req, res, function(){
-            req.flash("success", "Welcome to Yelp Camp " + user.username);
-            res.redirect("/campgrounds");
-        });
+    passport.authenticate("local")(req, res, function() {
+      req.flash("success", "Welcome to Yelp Camp " + user.username);
+      res.redirect("/campgrounds");
     });
+  });
 });
 
 //show login form
-router.get("/login", function(req, res){
-    res.render("login");
+router.get("/login", function(req, res) {
+  res.render("login");
 });
 
 //handle login  logic
-router.post("/login", passport.authenticate("local", {
+router.post(
+  "/login",
+  passport.authenticate("local", {
     successRedirect: "/campgrounds",
-    failureRedirect: "/login"
-}) , function(req, res){
-    
-});
+    failureRedirect: "/login",
+    failureFlash: true
+  }),
+  function(req, res) {}
+);
 
 //logout route
-router.get("/logout", function(req, res){
-    req.logOut();
-    req.flash("success", "Logged you out");
-    res.redirect("/campgrounds");
+router.get("/logout", function(req, res) {
+  req.logOut();
+  req.flash("success", "Logged you out");
+  res.redirect("/campgrounds");
 });
 
-router.get("/forgot", function(req, res){
-    res.render("forgot");
+router.get("/forgot", function(req, res) {
+  res.render("forgot");
 });
 
-router.post("/forgot", function(req, res){
-    async.waterfall([
-        function(done) {
-            crypto.randomBytes(20, function(err, buf){
-                var token = buf.toString("hex");
-                done(err, token);
-            });
-        },
-        function(token, done){
-            User.findOne({email: req.body.email}, function(err, user){
-                if(!user){
-                    req.flash("error", "No account associated with that email !!!");
-                    return res.redirect("/forot");
-                }
+router.post("/forgot", function(req, res) {
+  async.waterfall(
+    [
+      function(done) {
+        crypto.randomBytes(20, function(err, buf) {
+          var token = buf.toString("hex");
+          done(err, token);
+        });
+      },
 
-                user.resetPasswordToken = token;
-                user.resetPasswordExpires = Date.now() + 3600000; // 1hr
+      function(token, done) {
+        User.findOne({ email: req.body.email }, function(err, user) {
+          if (!user) {
+            req.flash("error", "No account associated with that email !!!");
+            return res.redirect("/forgot");
+          }
 
-                user.save(function(err){
-                    done(err, token, user);
+          user.resetPasswordToken = token;
+          user.resetPasswordExpires = Date.now() + 3600000; // 1hr
+
+          user.save(function(err) {
+            done(err, token, user);
+          });
+        });
+      },
+
+      function(token, user, done) {
+        var auth = {
+          type: "oauth2",
+          user: process.env.GMAIL,
+          clientId: process.env.CLIENT_ID,
+          clientSecret: process.env.CLIENT_SECRET,
+          refreshToken: process.env.REFRESH_TOKEN,
+          accessToken: process.env.ACCESS_TOKEN
+        };
+
+        var transporter = nodemailer.createTransport({
+          service: "Gmail",
+          auth: auth
+        });
+
+        var mailOptions = {
+          to: user.email,
+          from: process.env.GMAIL,
+          subject: "YelpCamp Password Reset",
+          text:
+            "You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n" +
+            "Please click on the following link, or paste this into your browser to complete the process:\n\n" +
+            "http://" +
+            req.headers.host +
+            "/reset/" +
+            token +
+            "\n\n" +
+            "If you did not request this, please ignore this email and your password will remain unchanged.\n"
+        };
+
+        transporter.sendMail(mailOptions, function(err) {
+          req.flash(
+            "success",
+            "An e-mail has been sent to " +
+              user.email +
+              " with further instructions."
+          );
+          done(err, "done");
+        });
+      }
+    ],
+    function(err) {
+      if (err) return next(err);
+      res.redirect("/forgot");
+    }
+  );
+});
+
+router.get("/reset/:token", function(req, res) {
+  User.findOne(
+    {
+      resetPasswordToken: req.params.token,
+      resetPasswordExpires: { $gt: Date.now() }
+    },
+    function(err, user) {
+      if (!user) {
+        req.flash("error", "Password reset token is invalid or has expired.");
+        return res.redirect("/forgot");
+      }
+      res.render("reset", { token: req.params.token });
+    }
+  );
+});
+
+router.post("/reset/:token", function(req, res) {
+  async.waterfall(
+    [
+      function(done) {
+        User.findOne(
+          {
+            resetPasswordToken: req.params.token,
+            resetPasswordExpires: { $gt: Date.now() }
+          },
+          function(err, user) {
+            if (!user) {
+              req.flash(
+                "error",
+                "Password reset token is invalid or has expired."
+              );
+              return res.redirect("back");
+            }
+
+            if (req.body.password === req.body.confirm) {
+              user.setPassword(req.body.password, function(err) {
+                user.resetPasswordToken = undefined;
+                user.resetPasswordExpires = undefined;
+
+                user.save(function(err) {
+                  req.logIn(user, function(err) {
+                    done(err, user);
+                  });
                 });
-            });
-        },
-        function(token, user, done){
-            var smtpTransport = nodemailer.createTransport({
-                service: "Gmail",
-                auth: {
-                    user: "taolaobidaomail@gmail.com",
-                    pass: process.env.GMAILPW
-                }
-            });
-
-            var mailOptions = {
-                to: user.email,
-                from: "taolaobidaomail@gmail.com",
-                subject: "YelpCamp Password Reset",
-                text: "You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n" +
-                "Please click on the following link, or paste this into your browser to complete the process:\n\n" +
-                "http://' + req.headers.host + '/reset/' + token + '\n\n" +
-                "If you did not request this, please ignore this email and your password will remain unchanged.\n"
-            };
-
-            smtpTransport.sendMail(mailOptions, function(err) {
-                req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
-                done(err, 'done');
               });
-        }
-    ], function(err){
-        if(err) return next(err);
-        res.redirect("/forgot");
-    });
+            } else {
+              req.flash("error", "Passwords do not match.");
+              return res.redirect("back");
+            }
+          }
+        );
+      },
+
+      function(user, done) {
+        var auth = {
+          type: "oauth2",
+          user: process.env.GMAIL,
+          clientId: process.env.CLIENT_ID,
+          clientSecret: process.env.CLIENT_SECRET,
+          refreshToken: process.env.REFRESH_TOKEN,
+          accessToken: process.env.ACCESS_TOKEN
+        };
+
+        var transporter = nodemailer.createTransport({
+          service: "Gmail",
+          auth: auth
+        });
+
+        var mailOptions = {
+          to: user.email,
+          from: process.env.GMAIL,
+          subject: "Your password has been changed",
+          text:
+            "Hello,\n\n" +
+            "This is a confirmation that the password for your account " +
+            user.email +
+            " has just been changed.\n"
+        };
+
+        transporter.sendMail(mailOptions, function(err) {
+          req.flash("success", "Success! Your password has been changed.");
+          done(err);
+        });
+      }
+    ],
+    function(err) {
+      res.redirect("/campgrounds");
+    }
+  );
 });
 
 module.exports = router;
